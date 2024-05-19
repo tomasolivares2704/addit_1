3ffe146fad841d6bf9a7387663814635fe9e7428
Gabriel Bracho <129797142+GBracho14@users.noreply.github.com>
2024-05-19 01:44:58 -0400

    Cambios en Tabs y Estilos de listas de compras

--- ToDoNow-main/App/src/app/pages/tabs/lista-compras/lista-compras.module.ts
+++ ToDoNow-main/App/src/app/pages/tabs/lista-compras/lista-compras.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { ListaComprasPageRoutingModule } from './lista-compras-routing.module';
 
 import { ListaComprasPage } from './lista-compras.page';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ListaComprasPageRoutingModule
+    ListaComprasPageRoutingModule,
+    SharedModule
   ],
   declarations: [ListaComprasPage]
 })
