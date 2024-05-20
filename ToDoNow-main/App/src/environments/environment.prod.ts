02e6bf1d40f7fb5c937b7e9a821c184fc4d0dff6
Gabriel Bracho <129797142+GBracho14@users.noreply.github.com>
2024-05-20 11:20:49 -0400

    Base de datos
    Optimizando las claves de acceso para cambio de las bases de datos

--- ToDoNow-main/App/src/environments/environment.prod.ts
+++ ToDoNow-main/App/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
+/*Base de Datos Tomás
 export const environment = {
   production: true,
   firebaseConfig: {
@@ -7,5 +8,19 @@ export const environment = {
   storageBucket: "addit10-5ceb4.appspot.com",
   messagingSenderId: "1053377253179",
   appId: "1:1053377253179:web:2da8c4936739706a4e311e"
+  }
+};*/
+
+/*Base de Datos Gabriel*/
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyClPY9NlZIU2YOQqSqIXvth92F9B1ScmgA",
+    authDomain: "addit-dbe28.firebaseapp.com",
+    projectId: "addit-dbe28",
+    storageBucket: "addit-dbe28.appspot.com",
+    messagingSenderId: "281057793578",
+    appId: "1:281057793578:web:298913b09fe59b10239553",
+    measurementId: "G-WY2HHNTZV3"
   }
 };
